59c9045d647ded5a2415531f5a6efbd412360770
marzec <gregmarzec02@gmail.com>
2022-09-02 17:00:17 -0400

    added apis

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,1 +1,39 @@
-// this is a comment
+export const addArtist = (userData) => {
+  return fetch("/api/artists", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+};
+
+export const addVenue = (userData) => {
+  return fetch("/api/venues", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+};
+
+export const artistLogin = (userData) => {
+  return fetch("/api/artists/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+};
+
+export const venueLogin = (userData) => {
+  return fetch("/api/venues/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+};
