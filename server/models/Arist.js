fdd5ed9ca475322638a6446717810841e0da0d20
marzec <gregmarzec02@gmail.com>
2022-09-02 13:58:04 -0400

    updated models for passwords

--- server/models/Arist.js
+++ server/models/Arist.js
@@ -22,7 +22,24 @@ const artistSchema = new Schema({
   number: {
     type: String,
   },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+  },
 });
+
+artistSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+artistSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const Artist = mongoose.model("Artist", artistSchema);
 
