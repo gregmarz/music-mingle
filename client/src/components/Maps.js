6798d7c82540cd2f071b16eddea3e06f15a8a3b5
Dr Dyl <dylkreisman@gmail.com>
2022-09-05 18:41:23 -0500

    google map layer

---
+++ client/src/components/Maps.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from 'react-google-maps/api'
+
+export default function Venue() {
+   const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_API_KEY
+   })
+   if (!isLoaded) return <div>Loading...</div>
+    return <Map />
+}
+
+function Map(){
+    const center= useMemo(() => ({lat: 25, lng:-80}))
+    return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName='map-container'>
+        <Marker position={center} />
+    </GoogleMap>
+    )
+}
