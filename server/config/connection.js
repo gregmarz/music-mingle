6dcaa5f835f99736fb437149bae5595d4abff582
marzec <gregmarzec02@gmail.com>
2022-09-05 17:49:42 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/music-mingle",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/music-mingle",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
-
 module.exports = mongoose.connection;
