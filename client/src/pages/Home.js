94eca10fb53c070960c6c75c75d355abf623686d
marzec <gregmarzec02@gmail.com>
2022-09-04 21:51:29 -0400

    added modals and homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,36 +2,54 @@
 import ArtistProf from "./ArtistProf"
 import VenueProf from "./VenueProf" */
 /* import "./src/App.css" */
-
+import React from "react";
 function Home() {
-    return (
-        <div>
-        <div>
-            <form className="arist-login">
-            <h2>Artist Login</h2>
-                <div>
-                    <label htmlFor="username">username</label>
-                    <input type="text" className="form-label" placeholder="username"></input>
-                </div>
-                <div>
-                    <label htmlFor="password">password</label>
-                    <input type="text" className="form-label" placeholder="email"></input>
-                </div>
-            </form>
-            <form>
-                <h2>Venue Login</h2>
-                <div>
-                    <label htmlFor="username">username</label>
-                    <input type="text" className="form-label" placeholder="username"></input>
-                </div>
-                <div>
-                    <label htmlFor="password">password</label>
-                    <input type="text" className="form-label" placeholder="email"></input>
-                </div>
-            </form>
-        </div>
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <form className="arist-login">
+          <h2>Artist Login</h2>
+          <div>
+            <label htmlFor="username">username</label>
+            <input
+              type="text"
+              className="form-label"
+              placeholder="username"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="password">password</label>
+            <input
+              type="text"
+              className="form-label"
+              placeholder="email"
+            ></input>
+          </div>
+          <button>Submit</button>
+        </form>
+        <form>
+          <h2>Venue Login</h2>
+          <div>
+            <label htmlFor="username">username</label>
+            <input
+              type="text"
+              className="form-label"
+              placeholder="username"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="password">password</label>
+            <input
+              type="text"
+              className="form-label"
+              placeholder="email"
+            ></input>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
