12db3fb3990545ed8435a0cc5d3fc0441b1002b2
marzec <gregmarzec02@gmail.com>
2022-09-05 17:52:17 -0400

    pushing for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/music-mingle",
+  process.env.MONGODB_URI || "mongodb://localhost/music-mingle",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   }
 );
+
 module.exports = mongoose.connection;
