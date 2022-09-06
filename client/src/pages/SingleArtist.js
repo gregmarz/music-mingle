06f12b4411f05919c58bc48b2a89943010e0b531
Shmuel Hoffman <snyh212@gmail.com>
2022-09-06 18:39:09 -0400

    changed artist and singleartist

--- client/src/pages/SingleArtist.js
+++ client/src/pages/SingleArtist.js
@@ -1,14 +1,30 @@
 import '../components/Nav/index.css'
 import React from "react";
+import { SAVE_ARTIST } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 function SingleArtist(item) {
     const {
+        _id,
         name,
         genre,
         groupSize,
         link
     } = item;
+
+    const [saveArtist, { error }] = useMutation(SAVE_ARTIST);
   
+    const handle = async (event) => {
+        event.preventDefault();
+        try {
+          await saveArtist({
+            variables: { _id: _id, name: name, genre: genre, groupSize: groupSize, link: link },
+          });
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
     return (
         <div className="div-border" key={name}>
         <ul className="card">
@@ -16,8 +32,9 @@ function SingleArtist(item) {
             <li className="deets">{genre}</li>
             <li className="deets">{groupSize}</li>
             <li className="deets">{link}</li>
-            <li className="deets"><button>Save Artist</button></li>
+            <li className="deets"><button onClick={handle}>Save Artist</button></li>
       </ul>
+      {error && <div>Something went wrong...</div>}
     </div>
     );
   }
