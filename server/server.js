d29b7f5d6a19dcdbd64ea36a61a10674c00541d1
Shmuel Hoffman <snyh212@gmail.com>
2022-09-07 11:35:21 -0400

    heroku push

--- server/server.js
+++ server/server.js
@@ -49,4 +49,4 @@ const startApolloServer = async (typeDefs, resolvers) => {
 };
 
 // Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers); 
